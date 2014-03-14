fc05b1c2f26cb7238ab1427db76786b475e6a1a1
Haotian Chai <haotianchai@wandoujia.com>
2014-03-14 14:53:18 +0800

    register half

--- upon/website/views.py
+++ upon/website/views.py
@@ -1,14 +1,15 @@
 #-*- coding:utf-8 -*-
 from django.shortcuts import render
 from django.contrib import auth
+from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponseRedirect
 # Create your views here.
 def login(request):
     errors = []
     if request.method == 'POST':
-        username = request.POST.get('email', '')
+        email = request.POST.get('email', '')
         password = request.POST.get('password', '')
-        user = auth.authenticate(username=username, password=password)
+        user = auth.authenticate(email=email, password=password)
         if user is not None and user.is_active:
             # Correct password, and the user is marked "active"
             auth.login(request, user)
@@ -20,7 +21,17 @@ def login(request):
     return render(request,'upon/login.html',{'errors':errors,})
 
 def register(request):
-    return render(request,'upon/register.html')
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            new_user = form.save()
+            return HttpResponseRedirect("upon/main.html")
+    else:
+        form = UserCreationForm()
+    return render(request,"upon/register.html", {
+        'form': form,
+    })
+    
 
 def main(request):
     return render(request,'upon/main.html')
