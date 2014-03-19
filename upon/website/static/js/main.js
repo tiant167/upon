0e22a088e585bd9dfeb76047985d491c6559718d
GaoZhiwei <847931364@qq.com>
2014-03-19 10:16:51 +0800

    project-list newproject-modal finish

--- upon/website/static/js/main.js
+++ upon/website/static/js/main.js
@@ -89,12 +89,13 @@
  });
  
   $("#project-list > a").click(function(){
-	//alert("success");
 	var all = $(this).parent().children().length - 1;
 	var parrent = $(this).index();
 	if(parrent < all) {
 		$(this).parent().children(":lt("+all+")").removeClass("active").children().remove();
 		$(this).addClass("active").append('<span class="glyphicon glyphicon-chevron-right"></span>');
+	} else {
+		$("#newproject-modal").modal('show');
 	}
  });
 
