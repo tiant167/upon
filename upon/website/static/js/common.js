442c747a3f8759e302d35d5b3ba1e885a624ce44
Haotian Chai <haotianchai@wandoujia.com>
2014-04-15 11:18:59 +0800

    half commit

---
+++ upon/website/static/js/common.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $('.finishbox').iCheck({
+        checkboxClass: 'icheckbox_square-blue',
+        radioClass: 'iradio_square-blue',
+        increaseArea: '20%' // optional
+    });
+    $('.confirmbox').iCheck({
+        checkboxClass: 'icheckbox_square-yellow',
+        radioClass: 'iradio_square-yellow',
+        increaseArea: '20%' // optional
+    });
+});
